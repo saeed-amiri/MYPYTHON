bcc2757ad6deffb9ad78b4d003beb6e2d5a2e024
Saeed Amiri <55943577+saeed-amiri@users.noreply.github.com>
2022-07-26 08:49:18 +0200

    Update mk_job_out.py

--- mk_job_out.py
+++ mk_job_out.py
@@ -1,80 +1,92 @@
 #!/usr/bin/env python3
 
-''' 
-Get average of all the simulation from "job.out". 
+'''
+Get average of all the simulation from "job.out".
 '''
 
-import os.path,os,sys
+import os
+import sys
+import os.path
+import time
 import numpy as np
 import pandas as pd
 from pathlib import Path
 import concurrent.futures
-import time
-# if os.path.exists('job.out') :
-  # fname = 'job.out'
-# elif os.path.exists('LOG.0'):
-  # fname = 'LOG.0'
-# else:
-  # print("read which:")
-  # os.system("echo $(ls -1v LOG.* )")
-  # fname = input()
-# print("reading: ",fname)
+
+#    if os.path.exists('job.out') :
+#     # fname = 'job.out'
+#   elif os.path.exists('LOG.0'):
+#    fname = 'LOG.0'
+#   else:
+#    print("read which:")
+#    os.system("echo $(ls -1v LOG.* )")
+#    fname = input()
+#   print("reading: ",fname)
+#
 start = time.perf_counter()
+
+
 def get_chunks(fname):
+    job = open(fname, "r")
+    joblines = job.readlines()
+    RUN: int = 0
+    LIENER: int = 0
+    LOOP: bool = False
+    WALL: bool = False
+    start = []
+    end = []
+    for line in reversed(joblines):
+        LIENER += 1
+        if line.startswith('Total wall time'):
+            RUN += 1
+            WALL = True
+        if line.startswith('Loop time of') and WALL:
+            end.append(LIENER+1)
+            LOOP = True
+            WALL = False
+        if line.startswith('Step') and LOOP:
+            start.append(LIENER)
+            if RUN == 1:
+                head = line.split()
+            LOOP = False
+    job.close()
+    print(f"---> {fname} contains {RUN} simulation(s)\n header:\n {head}")
+    return end, start, head, RUN, LIENER
 
-  job = open(fname,"r") 
-  joblines =job.readlines()
-  RUN: int = 0
-  LINER: int = 0
-  LOOP: bool = False
-  WALL: bool  = False
-  start=[];end=[]
-  for line in reversed(joblines):
-    LINER+=1
-    if line.startswith('Total wall time'):
-      RUN += 1
-      WALL = True
-    if (line.startswith('Loop time of') and WALL==True):
-      end.append(LINER+1)
-      LOOP = True
-      WALL = False
-    if (line.startswith('Step') and LOOP==True):
-      start.append(LINER)
-      if RUN==1: 
-        head=line.split()
-      LOOP = False
-  job.close()
-  print(f"---> {fname} contains {RUN} simulation(s)\n header:\n {head}")
-  
-  return end, start, head, RUN, LINER
+
 def make_df(fname):
-  # fname=os.path.join(src,'LOG.300')
-  end,start,head,RUN,l = get_chunks(fname)
-  start = [l-start[i]+1 for i in range(len(start))]
-  end = [l-end[i]+1 for i in range(len(end))]
-  start.reverse();end.reverse()
-  for i in range(RUN):
-    data = Path(fname).read_text().splitlines()[start[i]:end[i]]
-    df = [data[i].split() for i in range(len(data))]
-    df=np.asarray(df,dtype=float)
-    if i==0: index = df[:,0]
-    df=np.delete(df,0,1)
-    if i == 0: dfsum = df
-    else: dfsum+=df
-  header = head[1:]
-  index = np.asarray(index,dtype=int)
-  df = pd.DataFrame(dfsum/RUN,index=index,columns=header)
+    # fname=os.path.join(src,'LOG.300')
+    end, start, head, RUN, line = get_chunks(fname)
+    start = [line-start[i]+1 for i in range(len(start))]
+    end = [line-end[i]+1 for i in range(len(end))]
+    start.reverse()
+    end.reverse()
+    for i in range(RUN):
+        data = Path(fname).read_text().splitlines()[start[i]:end[i]]
+        df = [data[i].split() for i in range(len(data))]
+        df = np.asarray(df, dtype=float)
+        if i == 0:
+            index = df[:, 0]
+        df = np.delete(df, 0, 1)
+        if i == 0:
+            dfsum = df
+        else:
+            dfsum += df
+    header = head[1:]
+    index = np.asarray(index, dtype=int)
+    df = pd.DataFrame(dfsum/RUN, index=index, columns=header)
 
-  df.index.rename(head[0],inplace=True)
-  job_out=os.path.join('.','job_average' )
-  df.to_csv(job_out,sep='\t')
+    df.index.rename(head[0], inplace=True)
+    job_out = os.path.join('.', 'job_average')
+    df.to_csv(job_out, sep='\t')
+
 
 dirs = sys.argv[1:]
 # print(dirs)
 # dirs = [d for d in dirs if os.path.exists(os.path.join(d,'LOG.300'))]
 print(dirs)
 with concurrent.futures.ThreadPoolExecutor() as executor:
-    j = executor.map(make_df,dirs)
+    j = executor.map(make_df, dirs)
 
 finish = time.perf_counter()
 
