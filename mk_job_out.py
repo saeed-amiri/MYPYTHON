022f500754dad13a89a9943b833096892bfee63c
Saeed Amiri <55943577+saeed-amiri@users.noreply.github.com>
2022-07-26 08:08:09 +0200

    Update mk_job_out.py

--- mk_job_out.py
+++ mk_job_out.py
@@ -24,27 +24,31 @@ def get_chunks(fname):
 
   job = open(fname,"r") 
   joblines =job.readlines()
-  RUN  = 0; LINER = 0
-  LOOP = 0; WALL  = 0
+  RUN: int = 0
+  LINER: int = 0
+  LOOP: bool = False
+  WALL: bool  = False
   start=[];end=[]
   for line in reversed(joblines):
     LINER+=1
     if line.startswith('Total wall time'):
-      RUN+=1; WALL = 1
-    if (line.startswith('Loop time of') and WALL==1):
+      RUN += 1
+      WALL = True
+    if (line.startswith('Loop time of') and WALL==True):
       end.append(LINER+1)
-      LOOP = 1; WALL = 0
-    if (line.startswith('Step') and LOOP==1):
+      LOOP = True
+      WALL = False
+    if (line.startswith('Step') and LOOP==True):
       start.append(LINER)
       if RUN==1: 
         head=line.split()
-      LOOP = 0
+      LOOP = False
   job.close()
   print(f"---> {fname} contains {RUN} simulation(s)\n header:\n {head}")
   
   return end, start, head, RUN, LINER
-def make_df(src):
-  fname=os.path.join(src,'job.out')
+def make_df(fname):
+  # fname=os.path.join(src,'LOG.300')
   end,start,head,RUN,l = get_chunks(fname)
   start = [l-start[i]+1 for i in range(len(start))]
   end = [l-end[i]+1 for i in range(len(end))]
@@ -62,12 +66,13 @@ def make_df(src):
   df = pd.DataFrame(dfsum/RUN,index=index,columns=header)
 
   df.index.rename(head[0],inplace=True)
-  job_out=os.path.join(src,'job_average' )
+  job_out=os.path.join('.','job_average' )
   df.to_csv(job_out,sep='\t')
 
 dirs = sys.argv[1:]
+# print(dirs)
+# dirs = [d for d in dirs if os.path.exists(os.path.join(d,'LOG.300'))]
 print(dirs)
-dirs = [d for d in dirs if os.path.exists(os.path.join(d,'job.out'))]
 with concurrent.futures.ThreadPoolExecutor() as executor:
     j = executor.map(make_df,dirs)
 
